d1c3a5a2a3bd46beba1026be38753a84826022ae
张烟铃 <2581932570@qq.com>
2024-03-05 11:49:09 +0800

    merge

---
+++ examples/sites/demos/pc/app/float-button/basic-usage.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test'
+
+test('基本用法', async ({ page }) => {
+  page.on('pageerror', (exception) => expect(exception).toBeNull())
+  await page.goto('float-button#base')
+})
